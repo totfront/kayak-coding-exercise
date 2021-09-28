6346dcb09a390f99f3c0d73ce1d0af56af24d1ea
Alexandr Mir <totfront@gmail.com>
2021-09-28 15:00:22 +0200

    feat: onHover rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import '../styles/App.css'
 import Header from '../components/Header'
 import Airlines from './Airlines'
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,39 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import minimiseUrl from '../services/minimiseUrl'
+// import SmoothRender from 'react-smooth-render'
+
 import '../styles/blocks/card/card.scss'
 import '../styles/blocks/card/__title/card__title.scss'
 import '../styles/blocks/card/__logo/card__logo.scss'
 import '../styles/blocks/card/__site/card__site.scss'
-import '../styles/blocks/card/__phone-number/card__phone-number.scss'
+import '../styles/blocks/card/__text/card__text.scss'
+import '../styles/blocks/card/__text/_phone-number/card__text_phone-number.scss'
 
 export default function Card(props) {
-  const { data } = props
-  const { name, logoURL, site, phone, alliance } = data
-
+  const { name, logoURL, site, phone, alliance } = props.data
   const [siteUrl, setSiteUrl] = useState(site)
+  const [cardOnHover, setCardOnHover] = useState(false)
 
   useEffect(() => {
-    if (siteUrl) {
+    if (props.data) {
       minimiseUrl(siteUrl, setSiteUrl)
     }
   }, [])
 
-  // console.log(data)
-
   return (
-    <li className='card'>
+    <li className='card' onMouseOver={() => setCardOnHover(true)} onMouseLeave={() => setCardOnHover(false)}>
       <img className='card__logo' src={`https://content.r9cdn.net/${logoURL}`} alt='airline logo' />
       <article>
         <h3 className='card__title'>{name}</h3>
-        <span className='card__text'>{alliance === 'none' ? 'no alliance' : alliance}</span>
-        <address className='card__address'>
-          <a href={`tel:${phone}`} className='card__phone-number'>
-            {phone}
-          </a>
-        </address>
-        <a href={`${site}`} className='card__site'>
-          {siteUrl}
-        </a>
+        {cardOnHover && (
+          <>
+            {alliance !== 'none' && <span className='card__text'>{alliance}</span>}
+            <address className='card__address'>
+              <a href={`tel:${phone}`} className='card__text card__text_phone-number'>
+                {phone}
+              </a>
+            </address>
+            {/* <SmoothRender hidden={!cardOnHover} timing={300}> */}
+            <a href={`${site}`} className='card__site'>
+              {siteUrl}
+            </a>
+            {/* </SmoothRender> */}
+          </>
+        )}
       </article>
     </li>
   )
