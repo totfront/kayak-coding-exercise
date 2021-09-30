b0e16a2f4fcb5b51658548ce0bdc892f520c568d
Alexandr Mir <totfront@gmail.com>
2021-09-30 22:45:43 +0200

    feat: prepend elements

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,6 +3,8 @@ import '../styles/blocks/filter/__title/filter__title.scss'
 import '../styles/blocks/filter/__form/filter__form.scss'
 import '../styles/blocks/filter/__option/filter__option.scss'
 import '../styles/blocks/filter/__form-input/filter__form-input.scss'
+import '../styles/blocks/filter/__form-input-prepend/filter__form-input-prepend.scss'
+import '../styles/blocks/filter/__form-input-prepend/_selected/filter__form-input-prepend_selected.scss'
 
 export default function Filter(props) {
   const { heading, allAirlines, setAirlinePack } = props
@@ -17,7 +19,6 @@ export default function Filter(props) {
         ['SA', 'Star Alliance']
       ])
     )
-    console.log(airlines)
     return airlines.filter(airline => entries[airline.alliance] === selectedAlliance)
   }
 
@@ -25,7 +26,18 @@ export default function Filter(props) {
     setAirlinePack(filterAirlines(allAirlines, selectedAlliance))
   }, [selectedAlliance])
 
-  const handleChange = evt => setSelectedAlliance(evt.target.value)
+  const changePrependElementColor = (element, formContainer) => {
+    formContainer.querySelectorAll('.filter__form-input-prepend').forEach(prependElement => {
+      prependElement.classList.remove('filter__form-input-prepend_selected')
+    })
+
+    element.classList.add('filter__form-input-prepend_selected')
+  }
+
+  const handleChange = evt => {
+    setSelectedAlliance(evt.target.value)
+    changePrependElementColor(evt.target.parentNode.querySelector('.filter__form-input-prepend'), evt.target.parentNode.parentNode)
+  }
 
   return (
     <>
@@ -34,14 +46,17 @@ export default function Filter(props) {
         <form className='filter__form'>
           <label className='filter__option'>
             <input className='filter__form-input' onChange={handleChange} type='radio' name='filter' value='Oneworld' />
+            <span className='filter__form-input-prepend' />
             Oneworld
           </label>
           <label className='filter__option'>
             <input className='filter__form-input' onChange={handleChange} type='radio' name='filter' value='Sky Team' />
+            <span className='filter__form-input-prepend' />
             Sky Team
           </label>
           <label className='filter__option'>
             <input className='filter__form-input' onChange={handleChange} type='radio' name='filter' value='Star Alliance' />
+            <span className='filter__form-input-prepend' />
             Sky Alliance
           </label>
         </form>
